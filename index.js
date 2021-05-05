5f624205d038f0a4be7f31931f9561660531581a
Laura Messa Travin <laura.travin@gmail.com>
2021-05-05 00:47:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -82,7 +82,5 @@ function allWagesFor(employeeRecord){
 // }
 
 let findEmployeeByFirstName = function(srcArray, firstName) {
-
-let a = srcArray.map(x => x[0]).indexOf(firstName)
-
+  let a = srcArray.map(x => x.firstname).indexOf(firstName)
 }
